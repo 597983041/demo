f8b2e499f708e10b5e89a34b690ff584fe0eb710
597983041 <597983041@qq.com>
2023-05-30 18:01:11 +0800

    http接口

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@SpringBootApplication 标注这个类是一个springboot的应用
+@SpringBootApplication
+@MapperScan(basePackages = {"com.example.demo.mapper"})
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        //将springboot应用启动
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
